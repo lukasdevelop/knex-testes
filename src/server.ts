73400680d720c586f0693a19c2d1df7614554f9d
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-06-29 13:09:29 -0300

    testes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-import { transactionsRoutes } from './routes/transactions'
+import { app } from './app'
 import { env } from './env'
-const app = fastify()
-
-app.register(cookie)
-
-app.addHook('preHandler', async (request, reply) => {
-  console.log(`[${request.method}] ${request.url}`)
-})
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('server is running')
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,48 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, expect, test, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuario consegue criar uma nova transação', () => {
-  const responseStatusCode = 500
+describe('Transactions routes', () => {
 
-  expect(responseStatusCode).toEqual(201)
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 1000,
+      type: 'credit'
+    })
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async() => {
+    const createTransactionResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 1000,
+      type: 'credit'
+    })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsReponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+      expect(listTransactionsReponse.body.transactions).toEqual([
+        expect.objectContaining({
+          title: 'New Transaction',
+          amount: 1000,
+        })
+      ])
+  })
 })
